8e24db561c2ee84fe1e6f7f261c69745e807b5ba
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-08 22:17:48 +0000

    current file

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     for n in my_list:
-        if idx < 0 - 1:
+        if idx < 0:
             return none
         elif idx > len(my_list) - 1:
             return none
