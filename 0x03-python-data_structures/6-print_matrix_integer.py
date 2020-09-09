919cc930caf87e88a44734543bfe238d7a7d0614
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-09 01:16:35 +0000

    current file

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,9 @@ def print_matrix_integer(matrix=[[]]):
     for arr in matrix:
         i = 0
         for j in arr:
-            if i < len(arr):
+            if i < len(arr) - 1:
                 print("{:d} ".format(j), end='')
+            else:
+                print("{:d}".format(j), end='')
         i = i + 1
         print("")
