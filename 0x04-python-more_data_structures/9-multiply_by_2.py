08d67cfe5741d0f9f764b753d472c4efa7a8f0e8
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-11 04:05:04 +0000

    current file

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
     newn = {}
-    for mul in a_dictionary:
-        i = a_dictionary[mul] * 2
-        newn[mul] = i
+    for key in a_dictionary:
+        i = a_dictionary[key] * 2
+        newn[key] = i
     return new_n
