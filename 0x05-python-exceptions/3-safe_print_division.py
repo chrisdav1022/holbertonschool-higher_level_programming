816c8a3fdd247ee98c88206965f18e10b00e20a9
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-21 23:53:21 +0000

    current file

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+def safe_print_division(a, b):
+    i = None
+    try:
+        i = a / b
+    except ZeroDivisionError:
+        return None
+    finally:
+        print("Inside result: {}".format(i))
+        return i
