309c831b7ccb5528ea2d03aa02e56b6f56d03e2f
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-09-23 14:10:34 +0000

    current file

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 def list_division(my_list_1, my_list_2, list_length):
     nml = []
-    i= 0
+    i = 0
+
     for j in range(list_length):
         try:
             i = my_list_1[j] / my_list_2[j]
