100bafb462b2599902dae26acf05b8cd6e62fba4
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-07 23:49:29 +0000

    current file

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -2,14 +2,10 @@
 """read number lines"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """Function reads file"""
-    with open(filename, 'r') as fl:
-        if nb_lines <= 0:
-            print(fl.read(), end='')
-            return
-        ct = 0
-        for line in fl:
-            if ct < nb_lines:
-                print(line, end='')
-            ct += 1
+def number_of_lines(filename=""):
+    """c count the number of lines"""
+    c = 0
+    with open(filename, 'r') as f:
+        for line in f:
+            c += 1
+    return c
