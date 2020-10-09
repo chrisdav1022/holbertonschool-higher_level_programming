c5fcf503f220be2984931adb54b0b2acb55ae0f6
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-09 03:03:28 +0000

    current file

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""load, add, save"""
+
+
 from sys import argv
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
