d48f783ee3efb7aa31fdf801537b01f293a76b2a
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-13 21:25:32 +0000

    current file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 """Define a Rectangle class"""
+
+
 from models.base import Base
 
 
@@ -29,3 +31,77 @@ class Rectangle(Base):
                 raise ValueError('{} must be > 0'.format(name))
             if value < 0 and (name == 'x' or name == 'y'):
                 raise ValueError('{} must be >= 0'.format(name))
+        def area(self):
+        """Return the area of the rectangle."""
+        return self.__width * self.__height
+
+    def display(self):
+        """Print the rectangle."""
+        print('\n' * self.__y + (' ' * self.__x + '#' * self.__width + '\n') *
+              self.__height, end='')
+
+    def update(self, *args, **kwargs):
+        """Update the rectangle: id, width, height, x, y."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.__width = args[1]
+            if len(args) >= 3:
+                self.__height = args[2]
+            if len(args) >= 4:
+                self.__x = args[3]
+            if len(args) >= 5:
+                self.__y = args[4]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
+    def to_dictionary(self):
+        """Return dictionary representation of rectangle."""
+        return {'id': self.id, 'width': self.__width, 'height': self.__height,
+                'x': self.__x, 'y': self.__y}
+
+    @property
+    def width(self):
+        """Get the width."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Set the width."""
+        self.integer_validator('width', value)
+        self.__width = value
+
+    @property
+    def height(self):
+        """Get the height."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Set the height."""
+        self.integer_validator('height', value)
+        self.__height = value
+
+    @property
+    def x(self):
+        """Get x."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Set x."""
+        self.integer_validator('x', value)
+        self.__x = value
+
+    @property
+    def y(self):
+        """Get y."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Set y."""
+        self.integer_validator('y', value)
+        self.__y = value
