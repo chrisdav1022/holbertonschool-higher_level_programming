8411a546d2a90c7027bd92a4173016f0b9174d3d
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-10-13 21:46:22 +0000

    current file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """Define a Base class"""
-
 from os import path
 import json
-import csv
 
 
 class Base:
@@ -40,3 +38,56 @@ class Base:
             dictionary = [obj.to_dictionary() for obj in list_objs]
         with open(filename, 'w') as f:
             f.write(cls.to_json_string(dictionary))
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of instances."""
+        filename = cls.__name__ + '.json'
+        if path.isfile(filename):
+            with open(filename, 'r') as f:
+                my_list = cls.from_json_string(f.read())
+            return [cls.create(**obj) for obj in my_list]
+        return []
+
+    @classmethod
+    def create(cls, **dictionary):
+        """Return an instance with all attributes already set."""
+        if cls.__name__ == 'Rectangle':
+            new_base = cls(1, 1)
+        else:
+            new_base = cls(1)
+        cls.update(new_base, **dictionary)
+        return new_base
+
+    @classmethod
+    def save_to_file_csv(cls, list_objs):
+        """Write the CSV representation of list_objs to a file."""
+        filename = cls.__name__ + '.csv'
+        if list_objs is not None:
+            if cls.__name__ == 'Rectangle':
+                data = [[obj.id, obj.width, obj.height, obj.x, obj.y]
+                        for obj in list_objs]
+            else:
+                data = [[obj.id, obj.size, obj.x, obj.y] for obj in list_objs]
+            with open(filename, 'w', newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(data)
+
+    @classmethod
+    def load_from_file_csv(cls):
+        """Return a list of instances."""
+        filename = cls.__name__ + '.csv'
+        dictionary = []
+        if path.isfile(filename):
+            with open(filename, 'r') as f:
+                reader = csv.reader(f)
+                for row in reader:
+                    if cls.__name__ == 'Rectangle':
+                        data = {'id': int(row[0]), 'width': int(row[1]),
+                                'height': int(row[2]), 'x': int(row[3]),
+                                'y': int(row[4])}
+                    else:
+                        data = {'id': int(row[0]), 'size': int(row[1]),
+                                'x': int(row[2]), 'y': int(row[3])}
+                    dictionary.append(cls.create(**data))
+        return dictionary
--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 """Define a Rectangle class"""
-
-
 from models.base import Base
 
 
 class Rectangle(Base):
     """Define a Rectangle"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         """Initialize a Rectangle"""
         self.width = width
@@ -16,28 +13,30 @@ class Rectangle(Base):
         self.y = y
         super().__init__(id)
 
-        def __str__(self):
-            """Print the rectangle attributes"""
-            return ('[Rectangle] (' + str(self.id) + ') ' +
-                    str(self.__x) + '/' +
-                    str(self.__y) + ' - ' + str(self.__width) + '/' +
-                    str(self.__height))
+    def __str__(self):
+        """Print the rectangle attributes"""
+        return ('[Rectangle] (' + str(self.id) + ') ' +
+                str(self.__x) + '/' +
+                str(self.__y) + ' - ' + str(self.__width) + '/' +
+                str(self.__height))
 
-        def integer_validator(self, name, value):
-            """Validate value"""
-            if type(value) != int:
-                raise TypeError('{} must be an integer'.format(name))
-            if value <= 0 and (name == 'width' or name == 'height'):
-                raise ValueError('{} must be > 0'.format(name))
-            if value < 0 and (name == 'x' or name == 'y'):
-                raise ValueError('{} must be >= 0'.format(name))
-        def area(self):
+    def integer_validator(self, name, value):
+        """Validate value"""
+        if type(value) != int:
+            raise TypeError('{} must be an integer'.format(name))
+        if value <= 0 and (name == 'width' or name == 'height'):
+            raise ValueError('{} must be > 0'.format(name))
+        if value < 0 and (name == 'x' or name == 'y'):
+            raise ValueError('{} must be >= 0'.format(name))
+
+    def area(self):
         """Return the area of the rectangle."""
         return self.__width * self.__height
 
     def display(self):
         """Print the rectangle."""
-        print('\n' * self.__y + (' ' * self.__x + '#' * self.__width + '\n') *
+        print('\n' * self.__y +
+              (' ' * self.__x + '#' * self.__width + '\n') *
               self.__height, end='')
 
     def update(self, *args, **kwargs):
