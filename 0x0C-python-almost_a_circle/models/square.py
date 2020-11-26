817d805aa6985ed03e510bc8b01a8e32dea7010e
chrisdav1022 <chris_bedoya01@hotmail.com>
2020-11-26 04:47:10 +0000

    current file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -21,3 +21,31 @@ class Square(Rectangle):
         """Return dictionary representation of square."""
         return {'id': self.id, 'size': self.size, 'x': self._Rectangle__x,
                 'y': self._Rectangle__y}
+
+    @property
+    def size(self):
+        """Get the size."""
+        return self._Rectangle__width
+
+    @size.setter
+    def size(self, value):
+        """Set the size."""
+        self.integer_validator('width', value)
+        self.integer_validator('height', value)
+        self._Rectangle__width = value
+        self._Rectangle__height = value
+
+    def update(self, *args, **kwargs):
+        """Update the square: id, size, x, y."""
+        if args:
+            if len(args) >= 1:
+                self.id = args[0]
+            if len(args) >= 2:
+                self.size = args[1]
+            if len(args) >= 3:
+                self._Rectangle__x = args[2]
+            if len(args) >= 4:
+                self._Rectangle__y = args[3]
+        elif kwargs:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
