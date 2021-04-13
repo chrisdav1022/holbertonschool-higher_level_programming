fd9419efe560abd5895e97d211081e81e4680da6
christian bedoya <chrisdav1022>
2021-04-13 18:17:41 +0000

    current file

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,7 +2,7 @@
 """Module url"""
 import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status')as response:
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     body = response.read()
     print('Body response:\n\t- type: {}'.format(type(body)))
     print('\t- content: {}'.format(body))
