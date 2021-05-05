31f9e87f1fd4af05b676b8cd147baa29b69352cf
chrisdav1022 <chris_bedoya01@hotmail.com>
2021-05-05 03:39:50 +0000

    task complete

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+/*
+-The first argument is the API URL: https://jsonplaceholder.typicode.com/todos
+-Only print users with completed task
+*/
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
+  if (!error) {
+    const results = JSON.parse(body);
+    const objeto = {};
+    for (let i = 0; i < results.length; i++) {
+      if (objeto[results[i].userId] === undefined) {
+        objeto[results[i].userId] = 1;
+      } else if (results[i].completed === true) {
+        objeto[results[i].userId] += 1;
+      }
+    }
+    console.log(objeto);
+  }
