8b23b0896de8724068036f5e65e04c1629650d51
chrisdav1022 <chris_bedoya01@hotmail.com>
2021-05-05 03:26:22 +0000

    write me

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,5 +1,13 @@
 #!/usr/bin/node
 /*
--
--
+-The first argument is the file path
+-The second argument is the string to write
 */
+const fs = require('fs');
+const data = process.argv.slice(2, process.argv.length);
+
+fs.writeFile(data[0], data[1], (err) => {
+  if (err) { console.log(err); } else {
+    fs.readFileSync(data[0], 'utf8');
+  }
+});
